4027c181a3fa7695ea0d6b471f355f049a28513c
KingMunch <capfaiz@outlook.com>
2018-12-30 20:36:34 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#The file is about listing the attributes of a song (that i liked) using variables and printing it
+
+SongName = "Thank you, next" #The name of the song
+Artis = "Ariana Grande" #The name of an artist
+Genre = "Pop" #Type of genre
+ReleasedDateOnYoutube = "Nov 30 2018" #Date it is release
+DurationInSeconds = 330 #Long the video/song is
+CommentsInYoutube = 608626 #The amount of comments on the youtube page of the song
+ViewsInYoutube = 217916221 #The amount of views
+AmountOfLikes = 7.9e6 #The amount likes given(estimated)
+Random_Comment = 'athena: elizabeth gillies literally looks like lindsey lohan﻿' #A random comment from the music video
+
+print(SongName)
+print(Artis)
+print(Genre)
+print(ReleasedDateOnYoutube)
+print(DurationInSeconds)
+print(CommentsInYoutube)
+print(ViewsInYoutube)
+print(AmountOfLikes)
+print(Random_Comment)
