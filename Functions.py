5604d70b38463cda828b3791907e0ec51a2060d9
KingMunch <capfaiz@outlook.com>
2019-01-10 20:12:27 +0800

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,16 @@
+def Artist():
+	return 'Ariana Grande'
+
+def Genre():
+	return "Pop"
+
+def AmountOfLikes():
+	return 7.9e6
+
+def Awesome():
+	return True
+
+print(Artist())
+print(Genre())
+print(AmountOfLikes())
+print(Artist(), 'is awesome?' ,Awesome())
